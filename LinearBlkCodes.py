dc0f5b2c1715c75f97c01fbe3a287cf3fbed9e93
Julie.C <juliecycheng@gmail.com>
2018-08-14 20:20:52 +0800

    [develop] Encode and transmit

--- LinearBlkCodes.py
+++ LinearBlkCodes.py
@@ -16,3 +16,35 @@ class LDPC:
         H_matrix[H_matrix_row_col[:, 0], H_matrix_row_col[:, 1]] = 1
 
         return G_matrix, H_matrix
+
+def encode_and_transmit(G_matrix, SNR, batch_size, noise_io, intf_io, top_config, rng=0):
+    K, N = np.shape(G_matrix)
+
+    # Random generated binary message
+    if rng == 0:
+        x_bits = np.random.randint(0, 2, size=(batch_size, K))
+    else:
+        x_bits = rng.randint(0, 2, size=(batch_size, K))
+
+    # Encode
+    u_coded_bits = np.mod(np.matmul(x_bits, G_matrix), 2)  # shape: (B, N)
+
+    # BPSK
+    s_mod = u_coded_bits * (-2) + 1         # 0 to +1, 1 to -1
+
+    # Noise
+    ch_noise_normalize = noise_io.generate_noise(batch_size)
+    ch_noise_sigma = np.sqrt(1 / np.power(10, SNR / 10.0) / 2.0)
+    ch_noise = ch_noise_normalize * ch_noise_sigma
+
+    # Interference
+    intf_labels = intf_io.generate_intf_labels(batch_size)                          # shape: (B, 1)
+    intf_bits = np.random.randint(0, 2, size=(batch_size, top_config.intf_len))     # shape: (B, F)
+    intf_mod = intf_bits * (-2) + 1
+    intf_signal = np.zeros((batch_size, N))
+    intf_signal[:, 0:top_config.intf_len] = intf_mod[:, :]                          # shape: (B, N)
+
+    y_receive = s_mod + ch_noise + np.multiply(intf_signal, intf_labels)
+
+    LLR = y_receive * 2.0 / (ch_noise_sigma * ch_noise_sigma)
+    return x_bits, u_coded_bits, s_mod, ch_noise, intf_labels, y_receive, LLR

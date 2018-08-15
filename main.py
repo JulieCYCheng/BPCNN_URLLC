0045dc4fa9c64fecc23d4f34b146572cf6fb0728
Julie.C <juliecycheng@gmail.com>
2018-08-15 10:59:57 +0800

    [refactor] Typo "top_config"

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import DataIO
 import Iterative_BP_CNN as ibc
 
 # Get configuration
-top_cnofig = Configurations.TopConfig()
-top_cnofig.parse_cmd_line(sys.argv)
+top_config = Configurations.TopConfig()
+top_config.parse_cmd_line(sys.argv)
 
-train_config = Configurations.TrainConfig(top_cnofig)
-net_config = Configurations.NetConfig(top_cnofig)
+train_config = Configurations.TrainConfig(top_config)
+net_config = Configurations.NetConfig(top_config)
 
-code = lbc.LDPC(top_cnofig.N, top_cnofig.K, top_cnofig.G_file, top_cnofig.H_file)
+code = lbc.LDPC(top_config.N, top_config.K, top_config.G_file, top_config.H_file)
 
-if top_cnofig.function == 'GenData':
-    noise_io = DataIO.NoiseIO(top_cnofig, read_from_file=False, noise_file=None)
-    intf_io = DataIO.IntfIO(top_cnofig, read_from_file=False, intf_file=None)
+if top_config.function == 'GenData':
+    noise_io = DataIO.NoiseIO(top_config, read_from_file=False, noise_file=None)
+    intf_io = DataIO.IntfIO(top_config, read_from_file=False, intf_file=None)
 
     # Generate training data
-    ibc.generate_noise_samples(code, top_cnofig, train_config, net_config, 'Training', top_cnofig.BP_iter_nums_gen_data,
-                               top_cnofig.currently_trained_net_id, top_cnofig.model_id, noise_io, intf_io)
-    ibc.generate_noise_samples(code, top_cnofig, train_config, net_config, 'Test', top_cnofig.BP_iter_nums_gen_data,
-                               top_cnofig.currently_trained_net_id, top_cnofig.model_id, noise_io, intf_io)
+    ibc.generate_noise_samples(code, top_config, train_config, net_config, 'Training', top_config.BP_iter_nums_gen_data,
+                               top_config.currently_trained_net_id, top_config.model_id, noise_io, intf_io)
+    ibc.generate_noise_samples(code, top_config, train_config, net_config, 'Test', top_config.BP_iter_nums_gen_data,
+                               top_config.currently_trained_net_id, top_config.model_id, noise_io, intf_io)
 
     print("Finish GenData!")
 
